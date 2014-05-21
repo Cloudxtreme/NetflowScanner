1bef55148c1d8e0a249efe343b4a55cfe1b9ea14
NeverM0re <wuzehuai@gmail.com>
2014-05-21 18:10:44 +0800

    添加分类统计信息

---
+++ src/DisPane.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: garfield
+ * Date: 14-5-21
+ * Time: 下午6:09
+ * To change this template use File | Settings | File Templates.
+ */
+public class DisPane extends JPanel{
+    public DisPane(){
+        super();
+    }
+}
---
+++ src/ExceptionPane.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: garfield
+ * Date: 14-5-21
+ * Time: 下午6:06
+ * To change this template use File | Settings | File Templates.
+ */
+public class ExceptionPane extends JPanel{
+    public ExceptionPane(){
+        super();
+    }
+}
