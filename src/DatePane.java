5ddbd1b9fa3cda91e933547303af6fa96e449aee
NeverM0re <wuzehuai@gmail.com>
2014-05-22 00:12:37 +0800

    完成文字展示界面

--- src/DatePane.java
+++ src/DatePane.java
@@ -49,37 +49,52 @@ public class DatePane extends JPanel {
     }
 
     private class SearchPanel extends JPanel {
-        JPanel container = new JPanel();
+        private JPanel container = new JPanel();
+        private JPanel firs_container = new JPanel();
+        private JPanel sec_container = new JPanel();
         private String[] type = {"全部" , "TCP" , "UDP"};
-        JComboBox typechoice = new JComboBox(type);
-        JLabel startLabel = new JLabel("起始IP：");
-        JTextField start = new JTextField(15);
-        JLabel endLabel = new JLabel("结束IP：");
-        JTextField end = new JTextField(15);
-        JButton search = new JButton("go!");
-        JList resultlist;
-        Vector<String> result = new Vector<String>();
+        private JLabel portLabel = new JLabel("端口号");
+        private JTextField port = new JTextField(5);
+        private JComboBox typechoice = new JComboBox(type);
+        private JLabel des_startLabel = new JLabel("目的IP段:");
+        private JTextField des_start = new JTextField(10);
+        private JLabel des_endLabel = new JLabel("~");
+        private JTextField des_end = new JTextField(10);
+        private JLabel src_startLabel = new JLabel("源IP段:");
+        private JTextField src_start = new JTextField(10);
+        private JLabel src_endLabel = new JLabel("~");
+        private JTextField src_end = new JTextField(10);
+        private JButton searchButton = new JButton("go");
+        private Object[][] result = {
+                {"192.168.1.1" , new Integer(80) , "192.168.2.2" , new Integer(5556) ,new String("TCP"), new Integer(5886)} ,
+                {"192.168.1.3" , new Integer(80), "192.168.2.4"  , new Integer(8888) , new String("UDP"),new Integer(25553)} ,
+        };
+        private String[] object = {"目的IP" , "目的端口", "源IP"  , "源端口" , "类型","包数量"};
+
         SearchPanel() {
             this.setLayout(new BorderLayout());
             this.setBorder(new TitledBorder("分类检索"));
-            container.setLayout(new FlowLayout(FlowLayout.LEFT));
-            container.add(typechoice);
-            container.add(startLabel);
-            container.add(start);
-            container.add(endLabel);
-            container.add(end);
-            container.add(search);
+            firs_container.setLayout(new FlowLayout(FlowLayout.LEFT));
+            firs_container.add(src_startLabel);
+            firs_container.add(src_start);
+            firs_container.add(src_endLabel);
+            firs_container.add(src_end);
+            firs_container.add(des_startLabel);
+            firs_container.add(des_start);
+            firs_container.add(des_endLabel);
+            firs_container.add(des_end);
+            sec_container.setLayout(new FlowLayout(FlowLayout.LEFT));
+            sec_container.add(typechoice);
+            sec_container.add(portLabel);
+            sec_container.add(port);
+            sec_container.add(searchButton);
+            container.setLayout(new GridLayout(2, 1));
+            container.add(firs_container);
+            container.add(sec_container);
             this.add(container, BorderLayout.NORTH);
-
-       for(int i=0;i<100;i++){
-                result.add("hi");
-            }
-
-            resultlist = new JList(result);
-            this.add(resultlist,BorderLayout.CENTER);
-            this.add(new JScrollPane(this.resultlist));
-
-
+            JTable table = new JTable(result, object);
+            JScrollPane scrollPane = new JScrollPane(table);
+            this.add(scrollPane, BorderLayout.CENTER);
         }
     }
 }
