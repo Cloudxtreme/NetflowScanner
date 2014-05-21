dba87ede8a7b48720b494d986cee836df5b42ce7
NeverM0re <wuzehuai@gmail.com>
2014-05-21 18:13:09 +0800

    清除分类信息，将分类信息移动到总流量面板中

--- src/TabPane.java
+++ src/TabPane.java
@@ -6,15 +6,12 @@
  * To change this template use File | Settings | File Templates.
  */
 
-import com.orsoncharts.graphics3d.swing.DisplayPanel3D;
 
 import javax.swing.*;
 import javax.swing.event.*;
-import java.awt.*;
 
 public class TabPane extends JTabbedPane {
     private static final long serialVersionUID = 1L;
-    private DisPane disPane;
     private ExceptionPane exceptionPane;
     private TotalPane totalPane;
     private GraphPane graphPane;
@@ -22,11 +19,9 @@ public class TabPane extends JTabbedPane {
     public TabPane() {
         super(JTabbedPane.NORTH);
         totalPane = new TotalPane();
-        disPane = new DisPane();
         graphPane = new GraphPane();
         exceptionPane = new ExceptionPane();
         this.add("总流量", totalPane);
-        this.add("分类统计",disPane);
         this.add("统计图", graphPane);
         this.add("异常日志",exceptionPane);
         this.addChangeListener(new ChangeListener() {
